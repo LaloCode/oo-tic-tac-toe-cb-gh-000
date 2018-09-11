8db69e245cf8435a80fa0bba4d79079db5ec7684
José Lerma <lalo_pichu22@hotmail.com>
2018-09-11 05:12:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,7 @@ class TicTacToe
   counter
   end
 
-  def current_player(board)
-  turn_count(board) % 2 == 0 ? 'X' : 'O'
+  def current_player
+  turn_count(@board) % 2 == 0 ? 'X' : 'O'
   end
 end
